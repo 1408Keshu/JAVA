d09e1bc39adcc0317bc14f5e7afd98f3e7ff20c3
keshav31 <keshavchauhangtx2004@gmail.com>
2025-01-10 00:03:15 +0530

    variable in java

---
+++ variables/main.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.print("Sum of three numbers is: ");
+        // Variables 
+
+        int num1=4;
+        int num2 =5;
+        int num3=6;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+}
