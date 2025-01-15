570c2a519f5b287c27f1547929cf306eeaa5e88d
keshav31 <keshavchauhangtx2004@gmail.com>
2025-01-15 21:50:39 +0530

    Literals in java

---
+++ Literals/main.java
@@ -0,0 +1,22 @@
+package Literals;
+
+public class main {
+    public static void main(String[] args) {
+        char ch='A';
+        byte age=20;
+        int age2=334;
+        long longValue=1231231313121L;
+        float floatValue=0.2332f;
+        double doubleValue=8798.98D;
+        boolean boolValue=true;
+        String str= "String";
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(longValue);
+        System.out.println(ch);
+        System.out.println(floatValue);
+        System.out.println(doubleValue);
+        System.out.println(boolValue);
+        System.out.println(str);
+    }
+}
