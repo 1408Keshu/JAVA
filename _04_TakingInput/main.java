e16148fcc9c56f420ce51941ff0b5c62a543ca05
keshav31 <keshavchauhangtx2004@gmail.com>
2025-01-15 22:11:02 +0530

    Taking Input in java

---
+++ _04_TakingInput/main.java
@@ -0,0 +1,20 @@
+package _04_TakingInput;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+//         System.out.println("Taking input from User:");
+        Scanner sc =new Scanner(System.in);
+//         System.out.println("Enter numer 1");
+//         // int a= sc.nextInt();
+//         float a= sc.nextFloat();
+//         System.out.println("enter number 2");
+//         // int 4b=sc.nextInt();
+//         float b =sc.nextFloat();
+//         float sum=a+b;
+//         System.out.println("The sum of the these number is :");
+// System.out.println(sum);
+boolean b1= sc.hasNextInt();
+System.out.println(b1);
+}
+}  
