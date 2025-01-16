f40fba6c91da5cf6fc2d9f354dde03432d749b1f
keshav31 <keshavchauhangtx2004@gmail.com>
2025-01-16 10:56:56 +0530

    expressions in java

---
+++ _05_expressions/main.java
@@ -0,0 +1,23 @@
+package _05_expressions;
+
+public class main {
+    //expressions =operands + operators;
+    // Operators = + , - ,/ ,* ,%;
+    // Operators = values, variables , numbers , quantity;
+
+    public static void main(String[] args) {
+        int age = 10;
+        int age2 = 10;
+        // age = age +1;
+        age++;
+        //age = age - 1;
+        age2--;
+
+        double money = 100;
+        money = money/3;
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(money);
+        
+    }
+}
