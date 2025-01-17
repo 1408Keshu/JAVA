a443ca618204d74150d2d2c047fff9f7eaae6a7c
keshav31 <keshavchauhangtx2004@gmail.com>
2025-01-17 11:36:22 +0530

    for loops in java

---
+++ _13_ForLoop/main.java
@@ -0,0 +1,22 @@
+package _13_ForLoop;
+
+public class main {
+    public static void main(String[] args) {
+        
+         //for loop = execute a block of code limited amount of time;
+            // for(int i =0;i<5;i++){
+            //     System.out.println("Hello Keshu ji");
+            // }
+
+            // for(int i=0 ;i<10;i++){
+            //     System.out.println(i);
+            // }
+
+            for(int i=10 ;i>=0;i--){
+                System.out.print(i+ " " );
+            }
+
+
+
+    }
+}
