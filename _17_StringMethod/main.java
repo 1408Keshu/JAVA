fb54d1182714953335e66ea8937356ccc9e3b215
keshav31 <keshavchauhangtx2004@gmail.com>
2025-01-18 22:43:17 +0530

    String method in java

---
+++ _17_StringMethod/main.java
@@ -0,0 +1,25 @@
+package _17_StringMethod;
+
+public class main {
+    public static void main(String[] args) {
+        
+        // String = a reference data type that can store one or Characters
+        // reference data types that access to usefull methods
+
+        String name= "    Keshava    ";
+
+        // boolean result = name.equalsIgnoreCase("keshav");
+        // boolean result = name.equalsIgnoreCase("bro");
+        // boolean result = name.isEmpty();
+        // String result = name.toUpperCase();
+        // String result = name.toLowerCase();
+        // String result = name.replace("a","u");
+        // int result = name.length();
+        // char result = name.charAt(1);
+        // int result = name.indexOf("a");
+        String result = name.trim();
+
+        System.out.println(result);
+
+    }
+}
