3a4c5b690ea75ce75e318777a9bf27f568c3034b
keshav31 <keshavchauhangtx2004@gmail.com>
2025-01-19 12:45:25 +0530

    final keyword in java

---
+++ _25_FinalKeyword/main.java
@@ -0,0 +1,9 @@
+package _25_FinalKeyword;
+
+public class main {
+    public static void main(String[] args) {
+        final double pi= 3.14159; // final keyword is like a const 
+        // double pi =4; //can't chnage the value after initialising 
+        System.out.println(pi);
+    }
+}
