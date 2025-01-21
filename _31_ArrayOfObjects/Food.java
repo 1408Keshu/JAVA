a68743364d5ebead64cc2f16fb662b0e19acdf7b
keshav31 <keshavchauhangtx2004@gmail.com>
2025-01-21 16:48:01 +0530

    Array in classes with java

---
+++ _31_ArrayOfObjects/Food.java
@@ -0,0 +1,8 @@
+package _31_ArrayOfObjects;
+
+public class Food {
+    String name;
+    Food(String name){
+        this.name= name;
+    }
+}
---
+++ _31_ArrayOfObjects/main.java
@@ -0,0 +1,23 @@
+package _31_ArrayOfObjects;
+
+public class main {
+    public static void main(String[] args) {
+        
+        // Food[] referigerator= new Food[3];
+        
+        Food food1= new Food("Pizza");
+        Food food2= new Food("Burger");
+        Food food3= new Food("Sandwich");
+        
+        Food[] referigerator= {food1,food2,food3};
+        
+        // referigerator[0] = food1;
+        // referigerator[1] = food2;
+        // referigerator[2] = food3;
+        
+        System.out.println(referigerator[0].name);
+        System.out.println(referigerator[1].name);
+        System.out.println(referigerator[2].name);
+        
+    }
+}
